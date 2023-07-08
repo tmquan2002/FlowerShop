19338483d3daecab907fd0752aa35c41b5409ba2
Tran Dinh Khanh <54855318+khanhdinh02@users.noreply.github.com>
2023-07-08 21:55:15 +0700

    map view

--- app/src/main/java/com/example/flowershop/activity/user/MapViewActivity.java
+++ app/src/main/java/com/example/flowershop/activity/user/MapViewActivity.java
@@ -1,16 +1,37 @@
 package com.example.flowershop.activity.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.flowershop.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapViewActivity extends AppCompatActivity {
+public class MapViewActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_view);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng shopPosition = new LatLng(10.7674231, 106.6939905);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(shopPosition));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(16));
+        googleMap.addMarker(new MarkerOptions()
+                        .position(shopPosition)
+                        .title("Flower Shop"));
     }
 }
