2e941d1d6a2a7097cdef0581028e3cf56725507c
tmquan <quantm.lqd@gmail.com>
2023-07-09 16:54:42 +0700

    Add send message

--- app/src/main/java/com/example/flowershop/activity/main/ChatBoxActivity.java
+++ app/src/main/java/com/example/flowershop/activity/main/ChatBoxActivity.java
@@ -3,6 +3,7 @@ package com.example.flowershop.activity.main;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,15 +11,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.flowershop.R;
 import com.example.flowershop.activity.admin.NoAnimationLinearLayoutManager;
 import com.example.flowershop.adapter.ChatBoxAdapter;
+import com.example.flowershop.database.FirebaseDb;
+import com.example.flowershop.model.ChatUser;
+import com.example.flowershop.model.Message;
 import com.example.flowershop.model.UserRole;
 import com.example.flowershop.util.UserHelper;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatBoxActivity extends AppCompatActivity {
 
     ChatBoxAdapter adapter;
-    Button back;
+    Button back, send;
+    TextView messageSend;
     DatabaseReference base;
     private RecyclerView rv;
 
@@ -29,6 +35,8 @@ public class ChatBoxActivity extends AppCompatActivity {
 
         back = findViewById(R.id.backChatBtn);
         back.setOnClickListener(v -> finish());
+        send = findViewById(R.id.send);
+        messageSend = findViewById(R.id.type_message_block);
 
         rv = findViewById(R.id.recyclerView);
         base = FirebaseDatabase.getInstance().getReference();
@@ -41,5 +49,19 @@ public class ChatBoxActivity extends AppCompatActivity {
         }
         rv.setAdapter(adapter);
         rv.setLayoutManager(new NoAnimationLinearLayoutManager(this));
+
+        send.setOnClickListener(v -> {
+            if (UserHelper.getAuthUser().getRole() == UserRole.USER) {
+                Message message = new Message(messageSend.getText().toString(),false);
+                FirebaseDb.getInstance().messageDao().sendMessage(UserHelper.getAuthUser().getId(), message);
+                messageSend.setText("");
+            } else {
+                Message message = new Message(messageSend.getText().toString(),true);
+                Intent intent = getIntent();
+                int user_id = intent.getIntExtra("user_id", 2);
+                FirebaseDb.getInstance().messageDao().sendMessage(user_id, message);
+                messageSend.setText("");
+            }
+        });
     }
 }
